c0d72d87b24a6a771cff24e67a29f744284f53f6
Rishabh Pathak <clusmsyknight@gmail.com>
2024-02-15 19:20:57 +0530

    fixed faker import

--- src/utility/generateDummyTasks.ts
+++ src/utility/generateDummyTasks.ts
@@ -1,5 +1,5 @@
 // import fs from "fs";
-import { faker } from "@faker-js/faker";
+import { faker } from "@faker-js/faker/locale/en";
 import { Categories, Priority, TaskItemType, Tasks } from "src/@types/Task";
 
 const categories: Categories[] = ["ADDED", "STARTED", "COMPLETED"];
